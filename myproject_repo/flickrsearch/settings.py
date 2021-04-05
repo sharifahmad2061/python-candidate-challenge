3f11d64bcf0cc7c29c03f9a90e6814d2746bcca7
sharif ahmad <sharifahmad2061@gmail.com>
2021-04-05 12:15:18 +0500

    removed allauth

--- myproject_repo/flickrsearch/settings.py
+++ myproject_repo/flickrsearch/settings.py
@@ -37,10 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount'
 ]
 
 MIDDLEWARE = [
