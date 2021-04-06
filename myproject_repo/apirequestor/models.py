306b8d7be567e69b984408c54df417400d20d7d7
sharif ahmad <sharifahmad2061@gmail.com>
2021-04-06 12:28:30 +0500

    create and register model

--- myproject_repo/apirequestor/models.py
+++ myproject_repo/apirequestor/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=256)
+    place_id = models.CharField(max_length=64)
+    place_url = models.CharField(max_length=256)
+    latitude = models.DecimalField(max_digits=18, decimal_places=15)
+    longitude = models.DecimalField(max_digits=18, decimal_places=15)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "locations"
+
