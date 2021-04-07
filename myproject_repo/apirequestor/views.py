aff7b6a10e04a9f399d7a57fbe1ebeb07f6c9c9f
sharif ahmad <sharifahmad2061@gmail.com>
2021-04-07 12:42:40 +0500

    built the basic frontend layout for the application

--- myproject_repo/apirequestor/views.py
+++ myproject_repo/apirequestor/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.conf import settings
+
+import requests
+
+API_KEY = getattr(settings, 'FLICKR_API_KEY', None)
+API_URL = ("https://api.flickr.com/services/rest/"
+            "?method=flickr.places.findByLatLon"
+            "&api_key={}&lat={}&lon={}&format=json")
 
 # Create your views here.
 def index(request):
-    return render(request, 'apirequestor/index.html')
+    if request.method == 'GET':
+        return render(request, 'apirequestor/index.html')
+    elif request.method == 'POST':
+        latitude = request.POST.get('latitude')
+        longitude = request.POST.get('longitude')
+        url = API_URL.format(API_KEY, latitude, longitude)
+        print(url)
+        response = requests.get(url)
+        print(response)
+        return render(requests, 'apirequestor/index.html')
