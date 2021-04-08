863c4d0955cae7343511077b89ea68c32ca9492d
sharif ahmad <sharifahmad2061@gmail.com>
2021-04-08 12:36:38 +0500

    create the basic functionality which shows the pics from flick

--- myproject_repo/apirequestor/urls.py
+++ myproject_repo/apirequestor/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<lat>/<long>/<page>', views.index, name='home'),
-    path('photos/<int:page>', views.photos, name='next_page')
+    path('', views.index, name='home'),
+    path('photos/', views.photos, name='photos'),
 ]
--- myproject_repo/apirequestor/views.py
+++ myproject_repo/apirequestor/views.py
@@ -1,22 +1,49 @@
 from django.shortcuts import render
 from django.conf import settings
+from .models import Location
 
-import requests
+import requests as req_mod
 
 API_KEY = getattr(settings, 'FLICKR_API_KEY', None)
+
 API_URL = ("https://api.flickr.com/services/rest/"
-            "?method=flickr.places.findByLatLon"
-            "&api_key={}&lat={}&lon={}&format=json")
+            "?method=flickr.photos.search"
+            "&api_key={}&lat={}&lon={}&format=json&nojsoncallback=1&page={}&per_page=10")
+
+
+def call_api(lat, lon, page=1):
+    print(lat, lon, page)
+    url = API_URL.format(API_KEY, lat, lon, page)
+    response = req_mod.get(url).json()
+    # print(response)
+    context = {
+        'to': {
+            'photos': [],
+            'pages': response.get('photos').get('pages'),
+            'prev': response.get('photos').get('page') + 1 if response.get('photos').get('page') > 1 else None,
+            'page': response.get('photos').get('page'),
+            'lat': lat,
+            'lon': lon
+        }
+    }
+    for each_photo in response['photos']['photo']:
+        el  = {}
+        el['id'] = each_photo.get('id')
+        el['secret'] = each_photo.get('secret')
+        el['server'] = each_photo.get('server')
+        el['name'] = each_photo.get('title')
+        context.get('to').get('photos').append(el)
+    return context
 
 # Create your views here.
 def index(request):
     if request.method == 'GET':
         return render(request, 'apirequestor/index.html')
-    elif request.method == 'POST':
-        latitude = request.POST.get('latitude')
-        longitude = request.POST.get('longitude')
-        url = API_URL.format(API_KEY, latitude, longitude)
-        print(url)
-        response = requests.get(url)
-        print(response)
-        return render(requests, 'apirequestor/index.html')
+
+def photos(request):
+    if request.method == 'GET':
+        latitude = request.GET.get('latitude')
+        longitude = request.GET.get('longitude')
+        page_number = 1 if not request.GET.get('page') else request.GET.get('page')
+        context = call_api(lat=latitude, lon=longitude, page=page_number)
+        return render(request, 'apirequestor/index.html', context)
