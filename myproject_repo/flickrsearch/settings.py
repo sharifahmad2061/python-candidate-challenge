66bff451d1096b78943ec66390ba485e48613a85
sharif ahmad <sharifahmad2061@gmail.com>
2021-04-08 10:47:12 +0500

    brought in environment vars loading from wsgi to settings

--- myproject_repo/flickrsearch/settings.py
+++ myproject_repo/flickrsearch/settings.py
@@ -12,10 +12,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# load environment variables
+env_path = find_dotenv()
+load_dotenv(env_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
